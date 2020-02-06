e71bd2642310d0bf2804a605756f0d748c9fefe6
Euan Ramsay <euan.ramsay@ros.gov.uk>
2020-02-06 13:52:52 +0000

    Edited config.ts again.

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,9 +1,11 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
 const apiId = '07hl13ggw7'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const region = 'us-east-1'
+const stage = 'dev'
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/${stage}`
 
 export const authConfig = {
   domain: 'dev-hw08mj11.eu.auth0.com',
   clientId: 'y43wQj9pMbeWjB4FJOguCRY4tdK3BIIV',
-  callbackUrl: 'https://development.d1kkfs66jrggs0.amplifyapp.com/callback'
+  callbackUrl: '/callback'
 }
